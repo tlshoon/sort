ef548c1f60c23da90cae7e282eeec9e4bee1f3df
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-10-24 16:59:28 +0900

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -123,3 +123,27 @@
 #
 # location.sort()
 # print(location[(n-1)//2])
+
+# 실패율
+# N = int(input())
+# stages = list(map(int,input().split()))
+# def solution(N, stages):
+#     answer = []
+#     length = len(stages)
+#
+#     for i in range(1, N + 1):
+#         count = stages.count(i)
+#
+#         if length == 0:
+#             fail = 0
+#         else:
+#             fail = count / length
+#         answer.append((i, fail))
+#         length -= count
+#
+#     answer = sorted(answer, key=lambda t: t[1], reverse=True)
+#     answer = [i[0] for i in answer]
+#     return answer
+# print(solution(N,stages))
+
+
