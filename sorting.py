e5019c8cd611d7d44dd50cbc2b225f014e661002
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-12-03 19:42:29 +0900

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -146,4 +146,21 @@
 #     return answer
 # print(solution(N,stages))
 
-
+# 카드 정렬하기
+# import heapq
+# n = int(input())
+#
+# heap = []
+# for i in range(n):
+#     data = int(input())
+#     heapq.heappush(heap,data)
+#
+# result = 0
+#
+# while len(heap) != 1:
+#     one = heapq.heappop(heap)
+#     two = heapq.heappop(heap)
+#     sum_value = one + two
+#     result += sum_value
+#     heapq.heappush(heap, sum_value)
+# print(result)
